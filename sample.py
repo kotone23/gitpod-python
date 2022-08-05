6e9f70af519e55bc8ad013ea42a5bfbf2b057692
kotone23 <kotone-tanaka@kke.co.jp>
2022-08-05 03:36:39 +0000

    微修正

--- sample.py
+++ sample.py
@@ -1,6 +1,7 @@
-import 
-
+max = 4
 with open(file='recipients.csv', mode='w', encoding='utf-8') as f:
-    for i in range(5):
+    for i in range(max+1):
         email = 'user' + str(i) + '@example.com'
-        f.write(email + '\n')
+        f.write(email)
+        if not i == max:
+            f.write(',')
